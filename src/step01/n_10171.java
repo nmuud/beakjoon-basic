a5a2028b606e0c4e6552df1a090a6ff8d1ac9c0c
nmuud <nmuud@naver.com>
2024-09-14 18:04:06 +0900

    first commit

---
+++ src/step01/n_10171.java
@@ -0,0 +1,11 @@
+package step01;
+
+public class n_10171 {
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
