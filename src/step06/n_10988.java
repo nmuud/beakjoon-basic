c967ee067ca4e501c3c01ea42bc0edf41537e831
nmuud <nmuud@naver.com>
2024-09-20 21:17:39 +0900

    step 6

---
+++ src/step06/n_10988.java
@@ -0,0 +1,30 @@
+package step06;
+
+import java.util.Scanner;
+
+public class n_10988 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        String str = sc.next(); // 입력 단어 받기
+
+        char[] ch = str.toCharArray();
+
+        int lt = 0;
+        int rt = str.length() - 1;
+        int answer = 1; // 초기값을 1로 설정
+
+        while (lt < rt) {
+            if (ch[lt] != ch[rt]) {
+                answer = 0;
+                break;
+            }
+            lt++;
+            rt--;
+        }
+
+        System.out.println(answer);
+        sc.close();
+    }
+}
