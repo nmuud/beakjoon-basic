93ab7fdbbc794988c4ec89056c17eaf2517f0dd7
nmuud <nmuud@naver.com>
2024-09-22 21:19:03 +0900

    :)

---
+++ src/step08/n_2745.java
@@ -0,0 +1,23 @@
+package step08;
+
+import java.util.Scanner;
+
+public class n_2745 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String N = sc.next();
+        int B = sc.nextInt();
+
+        /*
+        Integer.parseInt(N, B)
+        문자열 N을 B진법 수로 해석하여, 10진법으로 변환해줌
+        */
+        int result = Integer.parseInt(N, B);
+
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
